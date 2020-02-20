ef67d705e37a51cf6bc49067149c5cc1fb774506
SofieLouise <sofma18@student.sdu.dk>
2020-02-20 10:14:21 +0100

    Prepare for lesson separation

---
+++ Lektion02/Weapon/src/main/java/dk/sdu/mmmi/cbse/bullet/BulletControlSystem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.bullet;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+/**
+ *
+ * @author Sofie
+ */
+public class BulletControlSystem implements IEntityProcessingService{
+
+    @Override
+    public void process(GameData gameData, World world) {
+        
+        world.getEntities(Bullet.class).forEach((bullet) -> {
+            PositionPart positionPart = bullet.getPart(PositionPart.class);
+            MovingPart movingPart = bullet.getPart(MovingPart.class);
+
+            movingPart.setLeft(true);
+            
+            movingPart.process(gameData, bullet);
+            positionPart.process(gameData, bullet);
+        });
+    }
+}
